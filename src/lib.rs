a455743290b5f3e2a23686734ea3b651daef09c0
antoniorea <antonioreacifuentes@yahoo.es>
2026-04-04 22:03:56 +0200

    first refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,21 @@ use std::f64::consts::PI;           //    const PI: f64 = 3.14159265359;
 use num_complex::Complex64;
 
 
-pub fn sum_complex( m1 : f64, a1 : f64, m2 : f64, a2 : f64) -> (Complex64 , Complex64) {
-    let z1 = Complex64::from_polar(m1, a1 * PI / 180.0);
-    let z2 = Complex64::from_polar(m2, a2 * PI / 180.0);
-    (z1 + z2, z1 - z2)    
+pub fn sum_complex( z1 : Complex64, z2 : Complex64) -> (Complex64 , Complex64) {
+    (z1 + z2, z1 - z2)
+}
+
+
+pub fn get_complex() -> Complex64 {
+    eprint!("Please enter the values for mudule and argument in degrees, separated with a space: \n>");
+    let mut holder = String::new();
+    std::io::stdin()
+        .read_line(&mut holder)
+        .expect("error reading input");
+    let test: Vec<&str> = holder.split_whitespace().collect();
+    assert_eq!(2, test.len(), "expected two numbers");
+    let m: f64 = test[0].parse().expect("'m' is not a number");
+    let a: f64 = test[1].parse().expect("'a' is not a number");
+    return Complex64::from_polar(m, a * PI / 180.0);
+
 }
