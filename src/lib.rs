b9475be5c40f00da2d6d3d98300e7c6a483d9f26
antoniorea <antonioreacifuentes@yahoo.es>
2026-05-18 21:53:43 +0200

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-#![allow(unused)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+use std::io::{self, Write};
 use std::f64::consts::PI;           //    const PI: f64 = 3.14159265359;
 use num_complex::Complex64;
 
@@ -20,4 +22,28 @@ pub fn get_complex() -> Complex64 {
     let a: f64 = test[1].parse::<f64>().expect("'a' is not a number");
     return Complex64::from_polar(m, a * PI / 180.0);
 
+}
+
+// Helper function to get input from the user
+pub fn get_input(prompt: &str) -> (f64, f64) {
+    loop {
+        println!("{}", prompt);
+        io::stdout().flush().unwrap();
+    
+        // 2. Leer la línea completa desde el teclado
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Error al leer la línea");
+        
+        let inputs: Vec<f64> = line.split_whitespace()
+            .map(|x| x.parse::<f64>().expect("Not a number!"))
+            .collect();
+        // inputs is a Vec<f64> of the inputs.
+        
+        match inputs.as_slice() {
+            [x, y] => return (*x, *y),
+            _ => println!("Se recibieron muy pocos argumentos"),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@ Second coplex number :
 z1 + z2:  El mod. de z es : 20.61; el arg. de z en grados es : 50.90    - Coord Cart. : 13.00 + 16.00 j
 z1 - z2:  El mod. de z es : 3.61; el arg. de z en grados es : 33.68     - Coord Cart. : 3.00 + 2.00 j
 */
-
+#![allow(dead_code)]
+#![allow(unused_variables)]
 use complex_rust;            // functions complex_rust::get_complex() and complex_rust::sum-complex
 use std::f64::consts::PI;           //    const PI: f64 = 3.14159265359;
 use num_complex::Complex64;
@@ -21,12 +22,14 @@ use num_complex::Complex64;
 
 fn main() {
     println!("Please enter the values for modulus and argument in degrees, separated with a space");
-    println!("First coplex number : ");
-    let  z1 : Complex64  = complex_rust::get_complex();
-    println!("Second coplex number : ");
-    let  z2 : Complex64  = complex_rust::get_complex();
+    let (m1, a1) = complex_rust::get_input("First coplex number : ");
+    let  z1 : Complex64  = Complex64::from_polar(m1, a1 * PI / 180.0);
+    let (m2, a2) = complex_rust::get_input("Second coplex number : ");
+    let  z2 : Complex64  = Complex64::from_polar(m2, a2 * PI / 180.0);
 
-    let (z, w) = complex_rust::sum_complex( z1, z2 );
+    //let (z, w) = complex_rust::sum_complex( z1, z2 );
+    let z = z1 + z2;
+    let w = z1 - z2;
 
     print!("\nz1 + z2:  El mod. de z es : {:.2}; el arg. de z en grados es : {:.2}", z.norm(), z.arg() * 180.0 / PI);
     println!("\t- Coord Cart. : {:.2} + {:.2} j", z.re, z.im);
